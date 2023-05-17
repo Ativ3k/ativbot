170206693c6bbf47e1ce2e9c6ff5993c057e33de
Ativ3k <dominik140401@gmail.com>
2023-05-17 03:20:39 +0200

    ecov3 - voice economy improvements, add daily coins for server boosters

--- SlashCommands/Ekonomia/MoneyVoice3.js
+++ SlashCommands/Ekonomia/MoneyVoice3.js
@@ -1,21 +1,20 @@
-const chalk = require('chalk');
-const moment = require('moment');
-const { Discord, EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const client = require('../../index');
 const emoji = require('../../json/emoji.json');
-const roles = require('../../json/roles.json');
 const db = require('../../Models/Eco');
 const guildSettings = require('../../Models/GuildSettings');
 const Logger = require('../../utils/logger');
 const { EcoVoiceAdd } = require('./utils/MoneyAdd');
 
 // todo
-client.on('ready', async (oldState, newState) => {
-  const interval = 3000;
+client.on('ready', async () => {
+  // voice
+  const intervalVoice = 3000;
   setInterval(async () => {
     const guilds = await guildSettings.find();
     guilds.forEach(async (guild) => {
       if (guild.ecoVC === '1') {
+        if (!client.guilds.cache.has(guild.GuildId)) return;
         const guildData = await client.guilds.cache.get(guild.GuildId);
         const guildMembers = guildData.members.cache.filter(
           (member) =>
@@ -25,9 +24,42 @@ client.on('ready', async (oldState, newState) => {
             !member.voice.mute,
         );
         guildMembers.forEach((member) => {
-          EcoVoiceAdd(guildData, member, guild, interval / 1000);
+          EcoVoiceAdd(guildData, member, guild, intervalVoice / 1000);
         });
       }
     });
-  }, interval);
+  }, intervalVoice);
+  // daily coins for boosting members
+  setInterval(async () => {
+    const time = new Date();
+    if (time.getHours() === 10 && time.getMinutes() === 0 && time.getSeconds() === 0) {
+      const guilds = await guildSettings.find();
+      guilds.forEach(async (guild) => {
+        if (guild.ecoVC === '1') {
+          if (!client.guilds.cache.has(guild.GuildId)) return;
+          const guildData = await client.guilds.cache.get(guild.GuildId);
+          const moneyValue = 200;
+          const serverBoosters = guildData.members.cache.filter((member) => member.premiumSince);
+
+          const embed = new EmbedBuilder()
+            .setDescription(
+              `${serverBoosters
+                .map((member) => member)
+                .slice(0, 175)
+                .sort((a, b) => a.premiumSinceTimestamp - b.premiumSinceTimestamp)
+                .join(', ')}`,
+            )
+            .setColor('Green');
+          serverBoosters.forEach(async (member) => {
+            const memberData = await db.findOne({ Memberid: member.id });
+            await memberData.updateOne({ $inc: { Money: moneyValue } });
+          });
+          guildData.systemChannel.send({
+            content: `Osoby wzmacniające serwer otrzymały ${moneyValue} ${emoji.jascoin}!`,
+            embeds: [embed],
+          });
+        }
+      });
+    }
+  }, 1000);
 });
