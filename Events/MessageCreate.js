5334ba037b83969cf3798da695d775319399d039
Ativ3k <dominik140401@gmail.com>
2023-06-08 02:51:43 +0200

    events: update events, delete old files

--- Events/MessageCreate.js
+++ Events/MessageCreate.js
@@ -1,55 +1,130 @@
+const { ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require('discord.js');
 const client = require('../index');
-
-const prefix = process.env.PREFIX;
 const emoji = require('../json/emoji.json');
+const dbPropozycje = require('../Models/propozycjedb');
 
 client.on('messageCreate', async (message) => {
+  // dobranocka
   const dobranocID = '159760934036963329';
   if (message.author.id === dobranocID && message.content.includes('Dobranocka')) {
     message.reply(`Dobranoc seksowny gnojku ❤${emoji.PEPEBEDGE}`);
   }
-
+  // ram usage
   if (message.author.id === '258891606873210880' && message.content.includes('!bench')) {
     const ram = process.memoryUsage().heapTotal / 1024 / 1024;
     message.reply(`result: ${ram}`);
   }
 
-  if (message.author.bot || !message.guild || !message.content.toLowerCase().startsWith(prefix)) return;
-
-  const args = message.content.slice(prefix.length).trim().split(' ');
-  const cmd = args.shift().toLowerCase();
-  const command = client.commands.get(cmd) || client.commands.find((c) => c.aliases?.includes(cmd));
-
-  if (!command) return;
-
-  if (command) {
-    // User Permissions Check
-    if (!message.member.permissions.has(command.userPermissions || []))
-      message.reply({
-        content: `${process.env.FAILURE_EMOJI} Potrzebujesz uprawnienia \`${
-          command.userPermissions || []
-        }\` aby użyc tej komendy`,
-        ephemeral: true,
-      });
-
-    // Under Maintenance Commands TODO (need fix)
-    // if (command.maintenance) {
-    //   if (!owners.includes(message.user.id)) {
-    //     message.reply({
-    //       content: `${process.env.FAILURE_EMOJI} Ta komenda jest jeszcze dopracowywana!`,
-    //     });
-    //   }
-    // }
-
-    // Bot Permissions Check
-    if (!message.guild.me.permissions.has(command.botPermissions || []))
-      message.reply({
-        content: `${process.env.FAILURE_EMOJI} Potrzebuje uprawnienia \`${
-          cmd.botPermissions || []
-        }\` aby wykonać tę komende`,
-        ephemeral: true,
-      });
-
-    await command.run(client, message, args); // Running the Command
+  // media channel
+  const imagevideo = message.attachments.filter(
+    (msg) => msg.contentType.includes('image') || msg.contentType.includes('video'),
+  ).size;
+  const image = message.attachments.filter((msg) => msg.contentType.includes('image')).size;
+  if (!message.channel.name) {
+    return;
   }
+  if (message) {
+    // memiki
+    if (message.channel.name.includes('memiki') && !message.author.bot) {
+      if (message.content.includes('.png')) {
+        return;
+      }
+
+      if (message.content.includes('.jpg')) {
+        return;
+      }
+
+      if (message.content.includes('.jpeg')) {
+        return;
+      }
+
+      if (message.content.includes('.gif')) {
+        return;
+      }
+
+      if (message.content.includes('prnt.sc')) {
+        return;
+      }
+
+      if (imagevideo === 0) {
+        message.delete();
+        return;
+      }
+
+      message.react(`${emoji.TAK}`);
+      message.react(`${emoji.MINUS}`);
+      message.react(`${emoji.NIE}`);
+      message.startThread({
+        name: `Komentarze`,
+        autoArchiveDuration: 1440,
+        type: 'GUILD_PUBLIC_THREAD',
+      });
+    }
+    // selfie
+    if (message.channel.name.includes('selfie') && !message.author.bot) {
+      if (message.content.includes('.png')) {
+        return;
+      }
+
+      if (message.content.includes('.jpg')) {
+        return;
+      }
+
+      if (message.content.includes('.jpeg')) {
+        return;
+      }
+
+      if (message.content.includes('.gif')) {
+        return;
+      }
+      if (message.content.includes('prnt.sc')) {
+        return;
+      }
+
+      if (image === 0) message.delete();
+    }
+  }
+  // end media channel
+
+  // propozycje
+  if (message.channel.id === '1019689485144965191' && !message.author.bot) {
+    const msgAuthor = message.author;
+    const msgContent = message.content;
+    await message.delete();
+    const PROPOZYCJE = client.channels.cache.get('1019689485144965191');
+
+    const embed = new EmbedBuilder()
+      .setTitle(`Propozycja | ${msgAuthor.username}`)
+      .setDescription(`${msgContent}`)
+      .setFields(
+        { name: `‎`, value: `${emoji.TAK}\n 0`, inline: true },
+        { name: `‎`, value: `${emoji.MINUS}\n 0`, inline: true },
+        { name: `‎`, value: `${emoji.NIE}\n 0`, inline: true },
+      )
+      .setColor('#303236')
+      .setFooter({
+        text: `${msgAuthor.username} + ${msgAuthor.id}`,
+      });
+    const button = new ActionRowBuilder()
+      .addComponents(new ButtonBuilder().setCustomId('propozycjeyes').setStyle('Success').setEmoji(`${emoji.TAK}`))
+      .addComponents(new ButtonBuilder().setCustomId('propozycjeidk').setStyle('Primary').setEmoji(`${emoji.MINUS}`))
+      .addComponents(new ButtonBuilder().setCustomId('propozycjeno').setStyle('Danger').setEmoji(`${emoji.NIE}`))
+      .addComponents(
+        new ButtonBuilder().setCustomId('propozycjedelete').setStyle('Secondary').setEmoji(`${emoji.TRASH}`),
+      )
+      .addComponents(new ButtonBuilder().setCustomId('propozycjeinfo').setStyle('Secondary').setEmoji(`❓`));
+
+    await PROPOZYCJE.send({ embeds: [embed], components: [button] });
+    const lastmsg = await PROPOZYCJE.messages.fetch({ limit: 1 });
+    await lastmsg.at(0).startThread({
+      name: `Komentarze`,
+      autoArchiveDuration: 10080,
+      type: 'GUILD_PUBLIC_THREAD',
+    });
+    await new dbPropozycje({
+      authorid: msgAuthor.id,
+      messageid: lastmsg.at(0).id,
+    }).save();
+  }
+  // end propozycje
 });
--- Events/Ready.js
+++ Events/Ready.js
@@ -169,4 +169,40 @@ client.on('ready', async () => {
     'Europe/Warsaw',
   );
   ciszaON.start();
+
+  setInterval(() => {
+    const gierki = [
+      '970797823987699773',
+      '736455324206104640',
+      '736455337904832612',
+      '736455359014764674',
+      '736455355768242237',
+      '736455320796266550',
+      '736455334838796289',
+      '736455331672227910',
+      '872091864994566184',
+      '872092402146504734',
+      '736455328149012500',
+      '736455343646703707',
+      '872092714202697758',
+      '872092763112505394',
+      '736455365415272560',
+      '872092550343835688',
+      '736455362122874941',
+      '872092817198043216',
+    ];
+    gierki.forEach((r) => {
+      const guild = client.guilds.cache.get('639632749610795009');
+      const role = guild.roles.cache.get(r);
+      const timeNow = new Date();
+      if (timeNow.getHours() === 7) {
+        console.log(role.mentionable);
+        guild.roles.edit(r, { mentionable: true });
+      }
+      if (timeNow.getHours() === 23) {
+        console.log(role.mentionable);
+        guild.roles.edit(r, { mentionable: false });
+      }
+    });
+  }, 1000);
 });
